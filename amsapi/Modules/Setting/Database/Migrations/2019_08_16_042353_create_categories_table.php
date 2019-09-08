03f376261b2296481173e0bd57eded67484700dc
Ariful Islam <ariful3010@gmail.com>
2019-09-08 19:58:16 +0600

    update

--- amsapi/Modules/Setting/Database/Migrations/2019_08_16_042353_create_categories_table.php
+++ amsapi/Modules/Setting/Database/Migrations/2019_08_16_042353_create_categories_table.php
@@ -17,7 +17,16 @@ class CreateCategoriesTable extends Migration
             $table->bigIncrements('id');
             $table->string('title')->nullable();
             $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->bigInteger('created_by')->nullable()->unsigned();
+            $table->bigInteger('updated_by')->nullable()->unsigned();
+            $table->softDeletes();
             $table->timestamps();
+        });
+
+        Schema::table('categories', function(Blueprint $table){
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
         });
     }
 
--- amsapi/Modules/Setting/Database/Migrations/2019_09_08_194913_create_topics_table.php
+++ amsapi/Modules/Setting/Database/Migrations/2019_09_08_194913_create_topics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAreasTable extends Migration
+class CreateTopicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateAreasTable extends Migration
      */
     public function up()
     {
-        Schema::create('areas', function (Blueprint $table) {
+        Schema::create('topics', function (Blueprint $table) {
             $table->bigIncrements('id');
-
+            $table->string('title')->nullable();
+            $table->bigInteger('parent_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateAreasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('areas');
+        Schema::dropIfExists('topics');
     }
 }
