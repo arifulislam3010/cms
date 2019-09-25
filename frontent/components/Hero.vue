df6812bcec2c70ede66dc52bd61e89bb276d59a4
Ariful Islam <ariful3010@gmail.com>
2019-09-25 06:29:09 +0600

    update

--- frontent/components/Hero.vue
+++ frontent/components/Hero.vue
@@ -1,71 +1,80 @@
 <template>
-     <!-- ##### Hero Area Start ##### -->
-    <div class="hero-area">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-lg-8">
-                    <!-- Breaking News Widget -->
-                    <div class="breaking-news-area d-flex align-items-center">
-                        <div class="news-title">
-                            <p>Breaking News</p>
-                        </div>
-                        <div class="breakingNews">
-                                    <marquee behavior="scroll" direction="left" scrollamount="4" onmouseover="stop()" onmouseout="start()">
-                                        <span>
-                                            <nuxt-link to="/">* Welcome to Colorlib Family</nuxt-link>
-                                        </span>
-
-                                        <span>
-                                            <nuxt-link to="/"> * Welcome to Colorlib Family</nuxt-link>
-                                        </span>
-                                    </marquee>
-                            
-                        </div>
-                    </div>
-
-                    <!-- Breaking News Widget -->
-                    <div class="breaking-news-area d-flex align-items-center mt-15">
-                        <div class="news-title title2">
-                            <p>International</p>
-                        </div>
-                        <div id="internationalTicker" class="ticker">
-                            <ul>
-                                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-                                <li><a href="#">Welcome to Colorlib Family.</a></li>
-                                <li><a href="#">Nam eu metus sitsit amet, consec!</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Hero Add -->
-                <HeroAdd />
+  <!-- ##### Hero Area Start ##### -->
+  <div class="hero-area">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12 col-lg-8">
+          <!-- Breaking News Widget -->
+          <div class="breaking-news-area d-flex align-items-center">
+            <div class="news-title">
+              <p>Breaking News</p>
             </div>
+            <div class="breakingNews">
+              <marquee
+                behavior="scroll"
+                direction="left"
+                scrollamount="4"
+                onmouseover="stop()"
+                onmouseout="start()"
+              >
+                <span>
+                  <nuxt-link to="/">* Welcome to Colorlib Family</nuxt-link>
+                </span>
+
+                <span>
+                  <nuxt-link to="/"> * Welcome to Colorlib Family</nuxt-link>
+                </span>
+              </marquee>
+            </div>
+          </div>
+
+          <!-- Breaking News Widget -->
+          <div class="breaking-news-area d-flex align-items-center mt-15">
+            <div class="news-title title2">
+              <p>International</p>
+            </div>
+            <div id="internationalTicker" class="ticker">
+              <ul>
+                <li>
+                  <a href="#"
+                    >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a
+                  >
+                </li>
+                <li><a href="#">Welcome to Colorlib Family.</a></li>
+                <li><a href="#">Nam eu metus sitsit amet, consec!</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
+
+        <!-- Hero Add -->
+        <HeroAdd />
+      </div>
     </div>
-    <!-- ##### Hero Area End ##### -->
+  </div>
+  <!-- ##### Hero Area End ##### -->
 </template>
 
 <script>
-import HeroAdd from './HeroAdd.vue'
+import HeroAdd from "./HeroAdd.vue";
 export default {
-    components:{HeroAdd},
+  components: { HeroAdd },
 
-    mounted() {
-        this.$nextTick(function () {
-    //   $.simpleTicker($("#breakingNewsTicker"), {
-    //     speed: 1000,
-    //     delay: 3000,
-    //     easing: 'swing',
-    //     effectType: 'roll'
-    // })
-  })
-    }
-}
+  mounted() {
+    this.$nextTick(function() {
+      //   $.simpleTicker($("#breakingNewsTicker"), {
+      //     speed: 1000,
+      //     delay: 3000,
+      //     easing: 'swing',
+      //     effectType: 'roll'
+      // })
+    });
+  }
+};
 </script>
 
 <style scoped>
-    .breakingNews {
-        width: 100%;
-    }
+.breakingNews {
+  width: 100%;
+}
 </style>
