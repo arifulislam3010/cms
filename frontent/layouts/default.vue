95ee7f3ca7b56e006978993de0a837c125bb2223
Ariful Islam <ariful3010@gmail.com>
2019-10-14 00:33:10 +0600

    update

--- frontent/layouts/default.vue
+++ frontent/layouts/default.vue
@@ -7,54 +7,8 @@
     <!-- Navbar Area -->
     
    <div>
-        <b-navbar toggleable="lg" type="dark" variant="danger">
-          <div class="container">
-                    <b-navbar-nav>
-                      <nuxt-link to="/" style="color: #fff; font-size: 17px;" class="btn btn-danger">Home</nuxt-link>
-                    </b-navbar-nav>
-
-                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                  <b-collapse id="nav-collapse" is-nav>
-                    
-                      <div v-if="!isHidden">
-                        <MainNavbar :categories = "categories"/>
-                      </div>
-
-                      <form class="form-inline ml-auto" v-show="isHidden">
-                          
-                              <div class="input-group mb-2 mr-sm-2">
-                                
-                                <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="search.." style="height:45px;font-size:14pt; width:800px;">
-                                <div class="input-group-prepend">
-                                  <div class="input-group-text">
-                                      <div class="pointer">
-                                        <font-awesome-icon :icon="['fas', 'search']"/>
-                                      </div>
-                                  </div>
-                                  <div class="input-group-text" style="margin-left: 5px;" @click="isHidden = !isHidden">
-                                      <div class="pointer">
-                                        <font-awesome-icon :icon="['fas', 'times']"/>
-                                      </div>
-                                  </div>
-                                </div>
-                              </div>
-                           
-                      </form>
-
-                      <!-- Right aligned nav items -->
-                          <b-navbar-nav class="ml-auto" @click="isHidden = !isHidden">
-                
-                                <div class="bg-danger text-light" style="cursor: pointer; font-size: 25px" v-if="!isHidden">
-                                  <font-awesome-icon :icon="['fas', 'search']"/>
-                                </div>
-                
-                    
-                          </b-navbar-nav>
-                  </b-collapse>
-          </div>
-      </b-navbar>
-      </div>
+     <MainNavbar :categories = "categories"/>  
+  </div>
   </header>
     <div>
       <nuxt />
@@ -65,6 +19,7 @@
 </template>
 
 <script>
+
 import axios from '@/plugins/axios'
 import Header from "~/components/Header"
 import TopHeader from "~/components/TopHeader"
@@ -104,6 +59,7 @@ export default {
     })
   },
   mounted() {            
+
       this.getCategories();
   },
  
